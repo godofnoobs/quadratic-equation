a2d4e62751affcefaecb1b8654af6d01ed9ed3e8
godofnoobs <n_ska@bk.ru>
2018-02-21 11:58:00 +0300

    prettify solvation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 module.exports = function solveEquation(equation) {
-//function solveEquation(equation) {
     var re = /[^\^]-?\s?\d+/g;
-    var args = equation.match(re)
-            .map(function (str) {return str.split(' ').join('');})
-            .map(function (str) {return parseInt(str);});
+    var args = equation.match(re);
+    args = args.map(function (str) {return parseFloat(str.replace(/\s+/g, ''));});
     var d = Math.pow(args[1],2) - 4*args[0]*args[2];
     var x1 = (-args[1]-Math.pow(d, 0.5))/(2*args[0]);
     var x2 = (-args[1]+Math.pow(d, 0.5))/(2*args[0]);
     res = [Math.round(x1), Math.round(x2)];
-    res.sort(function(a,b) {return a-b;});
+    if (x1 > x2)
+        res.reverse();
     return res;
 }
 
